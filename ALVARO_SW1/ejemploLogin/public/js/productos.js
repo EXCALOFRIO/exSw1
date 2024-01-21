863d8504986d4bba40694c4655ea35ca1da3ab47
EXCALOFRIO <alejandro.ramirezlarena@usp.ceu.es>
2024-01-21 14:23:31 +0100

    Add .gitignore and implement carrito functionality

---
+++ ALVARO_SW1/ejemploLogin/public/js/productos.js
@@ -0,0 +1,22 @@
+const product = [
+    { nombre: "Samsung S24", precio: 1800 },
+    { nombre: "Iphone 12", precio: 1500 },
+    { nombre: "Xiaomi Redmi Note 10", precio: 500 },
+    { nombre: "Huawei P40", precio: 900 },
+    { nombre: "OnePlus 9", precio: 700 },
+    { nombre: "Samsung S21", precio: 1000 },
+    { nombre: "Iphone 11", precio: 1200 },
+    { nombre: "Xiaomi Redmi Note 9", precio: 300 },
+    { nombre: "Huawei P30", precio: 700 },
+    { nombre: "OnePlus 8", precio: 500 },
+    { nombre: "Samsung S20", precio: 800 },
+    { nombre: "Iphone 10", precio: 1000 },
+    { nombre: "Xiaomi Redmi Note 8", precio: 200 },
+    { nombre: "Huawei P20", precio: 500 },
+    { nombre: "OnePlus 7", precio: 400 },
+    { nombre: "Samsung S10", precio: 600 },
+    { nombre: "Iphone 9", precio: 800 }
+    // ... otros productos
+];
+
+module.exports = product;
---
+++ ALVARO_SW1/ejemploLogin/routes/carrito.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const database2 = require('../database/models/user.model');
+
+
+router.get('/', function (req, res) {
+    // Obtener el nombre de usuario desde la sesión
+    const username = req.session.user.username;
+
+    try {
+        // Obtener el carrito del usuario desde la base de datos
+        const carrito = database2.getCart(username);
+
+        // Renderizar la vista con el carrito
+        res.render('carrito', { carrito: carrito || [], user: req.session.user });
+    } catch (error) {
+        // Manejar el error si el usuario no existe
+        console.error(error.message);
+        res.status(500).send('Error al obtener el carrito del usuario');
+    }
+});
+
+router.post('/', async (req, res) => {
+    // Obtener el producto pulsado desde la solicitud POST
+    const producto = req.body.producto;
+
+    // Verificar si el usuario tiene un carrito en la sesiónSS
+    if (!req.session.user.carrito) {
+        req.session.user.carrito = [];
+    }
+
+    // Agregar el producto al carrito del usuario
+    req.session.user.carrito.push(producto);
+
+    // Guardar el carrito en la base de datos o almacenamiento persistente
+    try {
+        await database2.saveCart(req.session.user.username, req.session.user.carrito);
+        // Respuesta al cliente
+        console.log('Sesión del usuario antes de renderizar la vista:', req.session.user);
+        res.render('carrito', { carrito: req.session.user.carrito || [], user: req.session.user });
+
+    } catch (error) {
+        // Manejar el error si hay un problema al guardar el carrito
+        console.error(error.message);
+        res.status(500).send('Error al guardar el carrito del usuario');
+    }
+});
+
+module.exports = router;
